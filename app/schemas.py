8478065ce99e1f8db562c023c5bf53073c908ad5
Renzo Lemmens <renzo@nas24.be>
2023-12-26 14:15:51 +0100

    Reformat file

--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel
 
+
 class NoteBase(BaseModel):
     title: str
     content: str
+
 
 class NoteUpdate(NoteBase):
     title: str | None = None
     content: str | None = None
 
+
 class NoteCreate(NoteBase):
     pass
+
 
 class Note(NoteBase):
     id: int
