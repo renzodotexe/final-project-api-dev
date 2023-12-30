cf9602d76d0f154520e96738f55c28cf1a56a8ce
Renzo Lemmens <renzo@nas24.be>
2023-12-30 22:49:58 +0100

    CORS

--- app/main.py
+++ app/main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
